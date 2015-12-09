0e1d0141a4774c10e28a1b4a8d275b037bd69812
Haoyu Wang <2411690987@qq.com>
2015-12-09 12:38:42 +0800

    完善了贪食蛇代码 修复了 #1
    close #1

--- js/snake.js
+++ js/snake.js
@@ -5,7 +5,8 @@ var snake = (function () {
     var Y_MAX = 20;
     var COLOR = "#33C";
     var FOOD_COLOR = "#d05257";
-    var TURN_SPEED = 120; // ms
+    var TEXT_COLOR = "#46619F";
+    var TURN_SPEED = 120  ; // ms
     var UP = 38;
     var DOWN = 40;
     var LEFT = 37;
@@ -18,17 +19,18 @@ var snake = (function () {
     };
 
     // 随机生成一个点
-    Point.random = function() {
+    Point.random = function () {
         var x = Math.round(Math.random() * (X_MAX - 2));
         var y = Math.round(Math.random() * (Y_MAX - 2));
         return new Point(x, y);
     };
 
-    Point.prototype.copy = function() {
+    Point.prototype.copy = function () {
         return new Point(this.x, this.y);
     };
 
-    Point.prototype.next = function(key) {
+    //判断点的下一个位置
+    Point.prototype.next = function (key) {
         var point = this.copy();
         if (key == UP) {
             point.y--;
@@ -52,7 +54,7 @@ var snake = (function () {
     var foods;
 
     // 处理响应键盘事件
-    function key_handler (event) {
+    function key_handler(event) {
         event.preventDefault();
         var new_key = event.keyCode;
         if (key == UP && new_key == DOWN) {
@@ -78,10 +80,31 @@ var snake = (function () {
         body = [];
         foods = [];
         body.push(Point.random());
-        for (var i=0; i<FOOD_NUM; i++) {
-            foods.push(Point.random())
+        for (var i = 0; i < FOOD_NUM; i++) {
+            foods.push(Point.random());
         }
+        $(document).unbind("keydown");
         $(document).keydown(key_handler);
+    }
+
+
+    function make_new_food() {
+        var new_food = Point.random();
+        var i;
+        var cell;
+        for (i=0; i<body.length;i++) {
+            cell = body[i];
+            if (new_food.x == cell.x && new_food.y == cell.y) {
+                return make_new_food();
+            }
+        }
+        for (i=0; i<foods.length;i++) {
+            food = foods[i];
+            if (new_food.x == food.x && new_food.y == food.y) {
+                return make_new_food();
+            }
+        }
+        return new_food;
     }
 
     function is_eat() {
@@ -90,7 +113,7 @@ var snake = (function () {
             var food = foods[i];
             if (next.x == food.x && next.y == food.y) {
                 body.unshift(food.copy());
-                foods[i] = Point.random();
+                foods[i] = make_new_food();
                 return true;
             }
         }
@@ -120,11 +143,6 @@ var snake = (function () {
         body[0] = next_point;
     }
 
-    function death() {
-        alert("你已经死了！重新来一把");
-        start();
-    }
-
     // 清除画布
     function clear() {
         pen.clearRect(0, 0, canvas.width, canvas.height);
@@ -140,14 +158,23 @@ var snake = (function () {
         pen.fillRect(x*SIZE, y*SIZE, SIZE, SIZE);
     }
 
+    //渲染画面
     function render() {
         clear();
-        for (i = 0; i < body.length; i++) {
-            draw_snake_cell(body[i].x, body[i].y);
+        if (key == undefined) {
+            pen.fillStyle = TEXT_COLOR;
+            pen.font = "24px serif";
+            pen.fillText("按方向键开始游戏", 10,70);
         }
 
-        for (i = 0; i < foods.length; i++) {
-            draw_food(foods[i].x, foods[i].y);
+        else {
+            for (i = 0; i < body.length; i++) {
+                draw_snake_cell(body[i].x, body[i].y);
+            }
+
+            for (i = 0; i < foods.length; i++) {
+                draw_food(foods[i].x, foods[i].y);
+            }
         }
     }
 
@@ -161,6 +188,7 @@ var snake = (function () {
             death();
             return;
         }
+
         render();
         setTimeout(loop, TURN_SPEED);
     }
@@ -171,7 +199,17 @@ var snake = (function () {
         loop();
     }
 
+    function death() {
+        clear();
+        pen.fillStyle = TEXT_COLOR;
+        pen.font = "24px serif";
+        pen.fillText("你己经死了!", 10, 40);
+        pen.fillText("按方向键重新开始", 10,70);
+        $(document).keydown(start);
+    }
+
     return {
         start: start
     }
 })();
+
